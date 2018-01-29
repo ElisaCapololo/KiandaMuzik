0722c07893a9df962c79ab14c6393f78f413c1e7
Elisa Capololo <elisacapololo2017@gmail.com>
2018-01-29 15:21:07 +0000

    Implementação de um clinte retrofit

--- app/src/main/java/com/example/elisacapololo/kiandamuzik/api/Client.java
+++ app/src/main/java/com/example/elisacapololo/kiandamuzik/api/Client.java
@@ -1,9 +1,19 @@
 package com.example.elisacapololo.kiandamuzik.api;
 
+import com.example.elisacapololo.kiandamuzik.api.models.Track;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 /**
  * Created by Elisa Capololo on 29-01-2018.
  */
 
 public class Client {
-
+    public interface PopularTracks {
+        @GET("popular")
+        Call<List<PopularTracks>> getPopularTracks();
+    }
 }
